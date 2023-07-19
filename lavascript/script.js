069c9e0956df8b341095ea213f51d65657d9e5fd
mochinamako <131327152+mochinamako@users.noreply.github.com>
2023-07-19 21:09:33 +0900

    Add files via upload

---
+++ lavascript/script.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('.slider1').bxSlider({
+        mode: 'fade',
+        speed: '5000',
+        auto: true,
+        autoControls: true,
+        pause: 5000,
+        slideMargin: 20
+    });
+});
+
+$(function () {
+    $('.slider2').bxSlider({
+        mode: 'fade',
+        speed: '5000',
+        auto: true,
+        autoControls: true,
+        pause: 5000,
+        slideMargin: 20
+    });
+});
